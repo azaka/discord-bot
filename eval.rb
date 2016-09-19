6d982885b13eaf1d149e56dfc9c527a9ec008326
azaka <yukkuri.shrine@gmail.com>
2016-09-20 00:18:18 +0800

    method for fetching recent repo issues

--- eval.rb
+++ eval.rb
@@ -40,4 +40,21 @@ bot.ready do |event|
 	# ready timestamp
 end
 
+# get recent issues for repos that matches keyword
+# as array of title strings
+
+require 'octokit'
+def recent_issue_titles(name)
+	#@@recent_issues = []
+	@@last_updated = nil
+	
+	issues = Octokit.search_issues name, {:sort => :updated, :per_page => 10}
+	
+	# filter for newer issues
+	issues.items.reject!{|issue_item| issue_item.updated_at < @@last_updated} unless @@last_updated.nil?
+	@@last_updated = issues.items.first.updated_at
+	
+	issues.items.map{|issue_item| issue_item.title}
+end
+
 bot.run
