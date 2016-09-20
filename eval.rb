bf62341f91810cbf81b9e2ce3ddaf765ee01cfef
azaka <yukkuri.shrine@gmail.com>
2016-09-20 23:10:34 +0800

    search recent repos by name

--- eval.rb
+++ eval.rb
@@ -44,17 +44,32 @@ end
 # as array of title strings
 
 require 'octokit'
+@last_updated = nil
 def recent_issue_titles(name)
 	#@@recent_issues = []
-	@@last_updated = nil
+	#@@last_updated = nil
 	
 	issues = Octokit.search_issues name, {:sort => :updated, :per_page => 10}
 	
 	# filter for newer issues
-	issues.items.reject!{|issue_item| issue_item.updated_at < @@last_updated} unless @@last_updated.nil?
-	@@last_updated = issues.items.first.updated_at
+	issues.items.reject!{|issue_item| issue_item.updated_at < @last_updated} unless @last_updated.nil?
+	@last_updated = issues.items.first.updated_at
 	
 	issues.items.map{|issue_item| issue_item.title}
+end
+
+# get recent updated repos that matches keyword
+# as array of title strings
+
+def recent_updated_titles(name)
+	issues = issue=Octokit.search_repositories name, {:sort => :updated}
+	
+	# filter for newer issues
+	issues.items.reject!{|issue_item| issue_item.updated_at < @last_updated} unless @last_updated.nil?
+	@last_updated = issues.items.first.updated_at
+	
+	issues.items.map{|issue_item| issue_item.title}
+	
 end
 
 bot.run
